09d675b12a904ea342192e32103a47ce6087ff50
dpatel51 <87962443+dpatel51@users.noreply.github.com>
2022-11-23 22:18:27 +0530

    Updated nav bar

--- src/ColorSchemesExample.js
+++ src/ColorSchemesExample.js
@@ -7,11 +7,11 @@ function ColorSchemesExample() {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Products</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Billing</Nav.Link>
+            <Nav.Link href="#pricing">Sales</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
